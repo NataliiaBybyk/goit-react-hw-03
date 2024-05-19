4196e1192b9a3e96b65372946ce6c5631955cf87
NataliiaBybyk <natashakorobochka@gmail.com>
2024-05-19 13:19:09 +0200

    init

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
-import './App.css';
-import ContactList from './components/ContactList/ContactList';
-import SearchBox from './components/SearchBox/SearchBox';
-import ContactForm from './components/ContactForm/ContactForm';
-import contacts from './contacts.json';
+
+import ContactList from '../ContactList/ContactList';
+import SearchBox from '../SearchBox/SearchBox';
+import ContactForm from '../ContactForm/ContactForm';
+import contacts from '../../contacts.json';
+import css from './App.module.css';
 
 function App() {
   const [contactList, setContactList] = useState(() => {
