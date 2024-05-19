52324ca9a6ce7e47b961f26beed09579b37fcc54
NataliiaBybyk <natashakorobochka@gmail.com>
2024-05-19 11:39:47 +0200

    init

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,14 @@
 import css from './Contact.module.css';
+import { FcTouchscreenSmartphone } from 'react-icons/fc';
 
 const Contact = ({ name, number, id, onDelete }) => {
   return (
     <div className={css.contact}>
       <p className={css.contactName}>Name: {name}</p>
-      <p className={css.contactNumber}>Number: {number}</p>
+      <p className={css.contactNumber}>
+        <FcTouchscreenSmartphone size={16} className={css.contact_descr_icon} />
+        Number: {number}
+      </p>
       <button
         className={css.deleteButton}
         type="button"
