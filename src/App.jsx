b53693d4b333a4d6939e25ed6361af6aae2ee930
NataliiaBybyk <natashakorobochka@gmail.com>
2024-05-19 11:56:13 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   );
 
   return (
-    <div className={css.container}>
+    <div>
       <h1>Phonebook</h1>
       <ContactForm onAddContact={addContact} />
       <SearchBox value={filter} onFilter={setFilter} />
